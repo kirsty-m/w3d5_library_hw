bb8416507b59e8535208a2a87d6d53634bc1ce45
Kirsty <kirstmcbride@gmail.com>
2023-06-04 14:17:05 +0100

    home page design included

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,7 +1,9 @@
 from flask import render_template, request, redirect
 from app import app
-from models.books import books, add_new_book
+from models.books import books, add_new_book, delete_book_by_index
 from models.book import *
+
+
 
 @app.route("/books")
 def index():
@@ -16,7 +18,17 @@ def add_book():
     book_title = request.form["title"]
     book_author = request.form["author"]
     book_genre = request.form["genre"]
-    new_book = Book(book_title, book_author, book_genre)
+    checked_out = []
+    new_book = Book(book_title, book_author, book_genre, checked_out)
     add_new_book(new_book)
-    return render_template("index.html", books = books)
+    # return render_template("index.html", books = books)
+    return redirect("/books")
+
+
+@app.route("/books/del/<index>", methods=["POST"])
+def delete_by_index(index):
+    delete_book_by_index(int(index))
+    return redirect("/books")
+    # return render_template("index.html", books = books)
+
 
--- models/books.py
+++ models/books.py
@@ -1,10 +1,13 @@
 from models.book import *
 
-book1 = Book("Great Expectations", "Charles Dickens", "Fiction")
-book2 = Book("Dune", "Frank Herbert", "Sci-fi")
-book3 = Book("Frankenstein", "Mary Shelley", "Horror")
+book1 = Book("Great Expectations", "Charles Dickens", "Fiction", True)
+book2 = Book("Dune", "Frank Herbert", "Sci-fi", False)
+book3 = Book("Frankenstein", "Mary Shelley", "Horror", True)
 
 books = [book1, book2, book3]
 
 def add_new_book(book):
     books.append(book)
+
+def delete_book_by_index(index):
+    books.pop(index)
